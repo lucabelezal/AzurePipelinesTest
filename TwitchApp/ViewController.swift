f9066d65fc35f8c62e67627f818f255d1a97910f
lucabelezal <lucabelezal@gmail.com>
2019-03-02 13:08:31 -0300

    Add TwitchApp project

---
+++ TwitchApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TwitchApp
+//
+//  Created by Lucas Nascimento on 26/01/19.
+//  Copyright © 2019 Lucas Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
